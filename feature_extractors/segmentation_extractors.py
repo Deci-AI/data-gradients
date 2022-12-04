2beeb1eea6a35982288128e980b30fafbbe296c1
tomer.keren <tomer.keren@deci.ai>
2022-12-04 16:47:14 +0200

    Added seg-count-small-objs feature extractor

--- feature_extractors/segmentation_extractors.py
+++ feature_extractors/segmentation_extractors.py
@@ -61,3 +61,30 @@ class SegmentationGetClassDistribution(FeatureExtractorBuilder):
 
         ax.grid(visible=True)
 
+
+class SegmentationCountSmallObjects(FeatureExtractorBuilder):
+    def __init__(self, train_set, params):
+        super().__init__(train_set)
+        min_pixels: int = int(512 * 512 / (params['percent_of_an_image'] * 100))
+        self.bins = np.array(range(0, min_pixels, int(min_pixels / 10)))
+        self._hist: List[int] = [0] * 11
+
+    def execute(self, data: BatchData):
+        for i, onehot_contours in enumerate(data.batch_onehot_contours):
+            for cls_contours in onehot_contours:
+                for c in cls_contours:
+                    _, _, contour_area = preprocessing.contours.get_contour_moment(c)
+                    self._hist[np.digitize(contour_area, self.bins) - 1] += 1
+
+    def process(self, ax):
+        label = ['<0.01', '0.02', '0.03', '0.04', '0.05', '0.06', '0.07', '0.08', '0.09', '0.1', '>0.1']
+
+        self._hist = list(np.array(self._hist) / sum(self._hist))
+
+        create_bar_plot(ax, self._hist, label,
+                        x_label="Object Size [%]", y_label="# Objects", ticks_rotation=0,
+                        title="Number of small objects", train=self.train_set)
+
+        ax.grid(visible=True, axis='y')
+
+        # ax.grid(visible=True)
