c71f4d0dad1a8da0c5c17fe08369e76904f82bc4
tomer.keren <tomer.keren@deci.ai>
2022-12-04 17:04:11 +0200

    refactored extractors

--- feature_extractors/general_extractors.py
+++ feature_extractors/general_extractors.py
@@ -3,7 +3,6 @@ from typing import List
 import numpy as np
 import torch
 import torchvision
-from matplotlib import pyplot as plt
 
 from batch_data import BatchData
 from feature_extractors.feature_extractor_abstract import FeatureExtractorBuilder
@@ -31,8 +30,8 @@ class NumberOfUniqueClasses(FeatureExtractorBuilder):
         super().__init__(train_set)
         self._unique_classes = set()
 
-    def execute(self, images, labels):
-        for label in labels:
+    def execute(self, data):
+        for label in data.labels:
             for val in label.unique():
                 self._unique_classes.add(val.item())
 
@@ -47,8 +46,8 @@ class ImagesResolutions(FeatureExtractorBuilder):
         self._res_dict = dict()
         self._channels_last = False
 
-    def execute(self, images, labels):
-        for image in images:
+    def execute(self, data):
+        for image in data.images:
             res = tuple(image.shape[:-1] if self._channels_last else image.shape[1:])
             if res not in self._res_dict:
                 self._res_dict[res] = 1
@@ -66,8 +65,8 @@ class ImagesAspectRatios(FeatureExtractorBuilder):
         self._ar_dict = dict()
         self._channels_last = False
 
-    def execute(self, images, labels):
-        for image in images:
+    def execute(self, data):
+        for image in data.images:
             res = tuple(image.shape[:-1] if self._channels_last else image.shape[1:])
             ar = res[0] / res[1]
             if ar not in self._ar_dict:
@@ -86,8 +85,8 @@ class AverageBrightness(FeatureExtractorBuilder):
         self._brightness: List[float] = []
         self._luminance: List[float] = []
 
-    def execute(self, images, labels):
-        for image in images:
+    def execute(self, data):
+        for image in data.images:
             self._brightness.append(np.round(torch.sum(image) / 3 / (image.shape[1] * image.shape[2]), 5))
             # self._luminance.append((0.2126 * image[0]) + (0.7152 * image[1]) + (0.0722 * image[2]))
 
@@ -102,10 +101,10 @@ class AverageContrast(FeatureExtractorBuilder):
         self._grayscale: bool = True
         self._contrast: List[float] = []
 
-    def execute(self, images, labels):
+    def execute(self, data):
         # if self._grayscale:
         #     images = self._get_gray_scaled_images()
-        images = [torchvision.transforms.ToPILImage()(x) for x in images]
+        images = [torchvision.transforms.ToPILImage()(x) for x in data.images]
         images = [torchvision.transforms.Grayscale()(x) for x in images]
         images = [torchvision.transforms.ToTensor()(x) for x in images]
         images = torch.stack(images)
@@ -121,11 +120,10 @@ class NumberOfBackgroundImages(FeatureExtractorBuilder):
         super().__init__(train_set)
         self._background_counter: int = 0
 
-    def execute(self, images, labels):
-        for label in labels:
+    def execute(self, data):
+        for label in data.labels:
             self._background_counter += 1 if torch.sum(label).item() == 0 else 0
 
     def process(self, ax):
         pass
         # print('Number of background images is: ', self._background_counter)
-
--- feature_extractors/segmentation_extractors.py
+++ feature_extractors/segmentation_extractors.py
@@ -9,31 +9,24 @@ from tensorboard_logger import create_bar_plot
 
 
 class SegmentationCountNumObjects(FeatureExtractorBuilder):
-    def __init__(self, train_set):
+    def __init__(self, train_set, params):
         super().__init__(train_set)
-        # 51 random number
-        self._hist: List[int] = [0] * 51
+        self._thresh = params['max_number_of_objects']
+        self._hist: List[int] = [0] * self._thresh
 
     def execute(self, data: BatchData):
         for onehot_contours in data.batch_onehot_contours:
             num_objects_per_image = 0
             for cls_contours in onehot_contours:
                 num_objects_per_image += len(cls_contours)
-            self._hist[num_objects_per_image] += 1
+            self._hist[min(num_objects_per_image, self._thresh)] += 1
 
     def process(self, ax):
-        # Cut hist from 51 (random number) to the highest # of objects found in data set
-        idx = len(self._hist)
-        for i, val in enumerate(reversed(self._hist)):
-            if self._hist[-i] > 0:
-                idx = len(self._hist) - i + 1
-                break
-
-        hist = self._hist[:idx]
         # Normalize hist
-        hist = list(np.array(hist) / sum(hist))
-
-        create_bar_plot(ax, hist, range(len(hist)), x_label="# Objects in image", y_label="# Of images",
+        hist = list(np.array(self._hist) / sum(self._hist))
+        # TODO: Fix hard coded labels number
+        labels = ['BG image', '1', '2', '3', '4', '5', '6', '7', '8', '9+']
+        create_bar_plot(ax, hist, labels, x_label="# Objects in image", y_label="# Of images",
                         title="# Objects per image", train=self.train_set)
 
         ax.grid(visible=True, axis='y')
@@ -65,6 +58,8 @@ class SegmentationGetClassDistribution(FeatureExtractorBuilder):
 class SegmentationCountSmallObjects(FeatureExtractorBuilder):
     def __init__(self, train_set, params):
         super().__init__(train_set)
+        # TODO: Do params validation before running program
+        assert 0 < params['percent_of_an_image'] < 100, "Param percent of image is a % of the image, in (0, 100)"
         min_pixels: int = int(512 * 512 / (params['percent_of_an_image'] * 100))
         self.bins = np.array(range(0, min_pixels, int(min_pixels / 10)))
         self._hist: List[int] = [0] * 11
@@ -77,6 +72,7 @@ class SegmentationCountSmallObjects(FeatureExtractorBuilder):
                     self._hist[np.digitize(contour_area, self.bins) - 1] += 1
 
     def process(self, ax):
+        # TODO: Fix hard-coded labels
         label = ['<0.01', '0.02', '0.03', '0.04', '0.05', '0.06', '0.07', '0.08', '0.09', '0.1', '>0.1']
 
         self._hist = list(np.array(self._hist) / sum(self._hist))
