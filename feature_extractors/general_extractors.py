e8b798e1e61e63507dacbc410dafa95446c20390
tomer.keren <tomer.keren@deci.ai>
2022-12-04 15:34:39 +0200

    Added number-of-images-and-labels feature extractor

---
+++ feature_extractors/general_extractors.py
@@ -0,0 +1,27 @@
+from typing import List
+
+import numpy as np
+import torch
+import torchvision
+from matplotlib import pyplot as plt
+
+from batch_data import BatchData
+from feature_extractors.feature_extractor_abstract import FeatureExtractorBuilder
+from tensorboard_logger import create_bar_plot
+
+
+class NumberOfImagesLabels(FeatureExtractorBuilder):
+    def __init__(self, train_set):
+        super().__init__(train_set)
+        self._num_images: int = 0
+        self._num_labels: int = 0
+
+    def execute(self, data: BatchData):
+        self._num_images += len(data.images)
+        self._num_labels += len(data.labels)
+
+    def process(self, ax):
+        create_bar_plot(ax=ax, data=[self._num_images, self._num_labels], labels=["images", "labels"],
+                        y_label='Total #', title='# Images & Labels', train=self.train_set,
+                        ticks_rotation=0)
+
