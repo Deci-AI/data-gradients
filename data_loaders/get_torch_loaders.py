bd67fb76550c47d8e4b91f0412a6ed7fea716e40
tomer.keren <tomer.keren@deci.ai>
2022-12-05 15:59:00 +0200

    added data loaders interface for self use

---
+++ data_loaders/get_torch_loaders.py
@@ -0,0 +1,125 @@
+import torchvision.transforms
+from torchvision import datasets, transforms
+from torchvision.datasets import Cityscapes, Kitti, VOCSegmentation
+from torchvision.transforms import ToTensor, CenterCrop
+from torch.utils.data import DataLoader
+
+from data_loaders.pp_humanseg_14k_dataset import PPHumanSegDataSet
+
+
+class DataLoaders:
+    def __init__(self):
+        self._transforms = torchvision.transforms.Compose([ToTensor(), CenterCrop(512)])
+        self._batch_size = 64
+
+    def _dataset_to_dataloader(self, dataset):
+        return DataLoader(dataset, batch_size=self._batch_size, shuffle=True)
+
+    def sbd(self):
+        train = datasets.SBDataset(
+            root="data/sbd",
+            image_set="train",
+            mode="segmentation",
+            download=False,
+            transforms=self._transforms
+        )
+
+        val = datasets.SBDataset(
+            root="data/sbd",
+            image_set="val",
+            mode="segmentation",
+            download=False,
+            transforms=self._transforms
+        )
+
+        train_dataloader = self._dataset_to_dataloader(train)
+        val_dataloader = self._dataset_to_dataloader(val)
+        return train_dataloader, val_dataloader
+
+    def pp_human(self):
+        dataset_root = "/Users/tomerkeren/workspace/deci-dataset-analyzer/data/pp_humanseg/PP-HumanSeg14K"
+        train = PPHumanSegDataSet(root=dataset_root,
+                                  image_set='train',
+                                  transform=self._transforms)
+
+        val = PPHumanSegDataSet(root=dataset_root,
+                                image_set='val',
+                                transform=self._transforms)
+        train_dataloader = self._dataset_to_dataloader(train)
+        val_dataloader = self._dataset_to_dataloader(val)
+        return train_dataloader, val_dataloader
+
+    def get_dataloader(self, dataset: str):
+        if dataset == "sbd":
+            return self.sbd()
+
+        elif dataset == "kitti":
+            raise NotImplementedError
+
+        elif dataset == "voc_seg":
+            raise NotImplementedError
+
+        elif dataset == "pp_human":
+            return self.pp_human()
+        else:
+            raise NotImplementedError
+
+
+sbd = True
+kitti = False
+voc_segmentation = False
+
+
+# elif kitti:
+#     def get_only_seg(target):
+#         print(target.keys())
+#         pass
+#
+#
+#     training_data = Kitti(root='data/Kitty',
+#                           download=True,
+#                           train=True,
+#                           transform=torchvision.transforms.Compose([ToTensor(), CenterCrop(512)]))
+#     val_data = Kitti(root='data/Kitty',
+#                      download=True,
+#                      train=False,
+#                      transform=torchvision.transforms.Compose([ToTensor(), CenterCrop(512)]),
+#                      target_transform=get_only_seg)
+
+# elif voc_segmentation:
+#     training_data = VOCSegmentation(root="data/VOC",
+#                                     image_set='train',
+#                                     download=True,
+#                                     transform=torchvision.transforms.Compose([ToTensor(), CenterCrop(512)]))
+#     val_data = VOCSegmentation(root="data/VOC",
+#                                image_set='val',
+#                                download=True,
+#                                transform=torchvision.transforms.Compose([ToTensor(), CenterCrop(512)]))
+# else:
+#     print("No dataset has been chosen")
+#     exit(0)
+#
+
+
+
+sbd_label_to_class = {0: 'aeroplane',
+                      1: 'bicycle',
+                      2: 'bird',
+                      3: 'boat',
+                      4: 'bottle',
+                      5: 'bus',
+                      6: 'car',
+                      7: 'cat',
+                      8: 'chair',
+                      9: 'cow',
+                      10: 'diningtable',
+                      11: 'dog',
+                      12: 'horse',
+                      13: 'motorbike',
+                      14: 'person',
+                      15: 'pottedplant',
+                      16: 'sheep',
+                      17: 'sofa',
+                      18: 'train',
+                      19: 'tvmonitor'
+                      }
---
+++ data_loaders/pp_humanseg_14k_dataset.py
@@ -0,0 +1,37 @@
+import os
+from typing import List
+
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+
+
+class PPHumanSegDataSet(Dataset):
+    CLASS_LABELS = {0: "background", 1: "person"}
+
+    def __init__(self, root, image_set, transform=None, target_transform=None):
+        self.root = root
+        self.images_labels = self._read_annotations_file(image_set)
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.images_labels)
+
+    def __getitem__(self, index):
+        current_line = self.images_labels[index].split(" ")
+        img_path = os.path.join(self.root, current_line[0].strip())
+        label_path = os.path.join(self.root, current_line[1].strip())
+        image = Image.open(img_path)
+        label = Image.open(label_path)
+        if self.transform is not None:
+            image = self.transform(image)
+        if self.transform is not None:
+            label = self.transform(label)
+        return image, label
+
+    def _read_annotations_file(self, image_set) -> List[str]:
+        file = os.path.join(self.root, f"{image_set}.txt")
+        with open(file) as f:
+            lines = f.readlines()
+        return lines
