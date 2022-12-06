bc3ef54d2cbaacc0409744b3b68ac51c1ae3f255
tomer.keren <tomer.keren@deci.ai>
2022-12-06 12:12:50 +0200

    added base class for loggers

---
+++ logger/results_logger.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+import datetime as dt
+
+
+class ResultsLogger(ABC):
+    def __init__(self):
+        self.logdir = "logs/train_data/" + dt.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    @abstractmethod
+    def log_graph(self, title: str, graph):
+        pass
+
+    @abstractmethod
+    def log_text(self, title: str, text):
+        pass
--- logger/tensorboard_logger.py
+++ logger/tensorboard_logger.py
@@ -1,24 +1,19 @@
-import datetime as dt
-from collections import OrderedDict
 
 from torch.utils.tensorboard import SummaryWriter
 
-
-def get_dict_ordered_by_values(dictionary):
-    dictionary = OrderedDict(sorted(dictionary.items()))
+from logger.results_logger import ResultsLogger
 
 
-class TensorBoardLogger:
+class TensorBoardLogger(ResultsLogger):
 
     def __init__(self):
-        logdir = "logs/train_data/" + dt.datetime.now().strftime("%Y%m%d-%H%M%S")
+        super().__init__()
+        self.writer = SummaryWriter(log_dir=self.logdir)
 
-        self.writer = SummaryWriter(log_dir=logdir)
-
-    def graph_to_tensorboard(self, title, plot):
+    def log_graph(self, title, plot):
         self.writer.add_figure(title, plot)
 
-    def text_to_tensorboard(self, title, text):
+    def log_text(self, title, text):
         self.writer.add_image(title, text)
 
     def close(self):
