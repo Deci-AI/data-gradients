344e44f5898f036e954329261c8dc1129ae852bf
tomer.keren <tomer.keren@deci.ai>
2022-12-08 13:45:38 +0200

    refactored onehot function

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,2 +1,2 @@
-from preprocessing.onehot import get_onehot
+from preprocessing.onehot import squeeze_by_classes
 from preprocessing.contours import get_contours
--- preprocessing/onehot.py
+++ preprocessing/onehot.py
@@ -1,16 +1,32 @@
 import torch
 
 
-def get_onehot(label) -> torch.Tensor:
+def squeeze_by_classes(label, is_one_hot) -> torch.Tensor:
+
+    # Take all classes but ignored/background
     all_classes = torch.unique(label)[1:]
 
+    # If no classes appear in the annotation it's a background image full of zeros
     if not all_classes.nelement():
+        # TODO If it's one hot I should handle it differently
         return label
 
     masks = []
-    for cls in all_classes:
-        mask = torch.where((label == cls) & (label > 0), torch.tensor(cls), torch.tensor(0))
-        masks.append(mask)
+    # TODO: Check that method works for one-hots
+    if is_one_hot:
+        # Iterate over channels
+        for i in range(label.shape[0]):
+            # If the channel exist in the annotations
+            if i in all_classes:
+                mask = label[i, ...]
+                mask = torch.where((mask > 0), torch.tensor(i), torch.zeros_like(mask))
+                masks.append(mask)
 
-    one_hot = torch.concat(masks, dim=0)
+    else:
+        for cls in all_classes:
+            mask = torch.where((label == cls) & (label > 0), torch.tensor(cls), torch.tensor(0))
+            masks.append(mask)
+
+    one_hot = torch.stack(masks, dim=0)
     return one_hot
+
