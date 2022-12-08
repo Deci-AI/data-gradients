f0cd554239d82f9b84ca316710fefc1a42a2ffbe
tomer.keren <tomer.keren@deci.ai>
2022-12-08 17:48:23 +0200

    Removed mapping

--- feature_extractors/__init__.py
+++ feature_extractors/__init__.py
@@ -1,15 +1,2 @@
 from feature_extractors.segmentation import *
 from feature_extractors.common import *
-
-FEATURE_EXTRACTORS = {
-    "SegmentationCountNumObjects": SegmentationCountNumObjects,
-    "SegmentationGetClassDistribution": SegmentationGetClassDistribution,
-    "SegmentationCountSmallObjects": SegmentationCountSmallObjects,
-    "NumberOfImagesLabels": NumberOfImagesLabels,
-    "NumberOfUniqueClasses": NumberOfUniqueClasses,
-    "ImagesResolutions": ImagesResolutions,
-    "ImagesAspectRatios": ImagesAspectRatios,
-    "AverageBrightness": AverageBrightness,
-    "AverageContrast": AverageContrast,
-    "NumberOfBackgroundImages": NumberOfBackgroundImages
-}
