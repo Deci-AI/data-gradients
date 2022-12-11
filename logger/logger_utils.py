2b66ea5055faab0c822acfc9743532a090cef941
tomer.keren <tomer.keren@deci.ai>
2022-12-11 14:53:45 +0200

    fixed tiny bug

--- logger/logger_utils.py
+++ logger/logger_utils.py
@@ -24,9 +24,9 @@ def create_bar_plot(ax, data, labels, x_label: str = "", y_label: str = "", titl
     ax.legend()
 
 
-def create_heatmap_plot(ax, x, y, bins=50, sigma=2, title="", x_label="", y_label="", gaussian_filter: bool=True):
+def create_heatmap_plot(ax, x, y, bins=50, sigma=2, title="", x_label="", y_label="", use_gaussian_filter: bool=True):
     heatmap, xedges, yedges = np.histogram2d(x, y, bins=bins)
-    if gaussian_filter:
+    if use_gaussian_filter:
         heatmap = gaussian_filter(heatmap, sigma=sigma)
     extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
     ax.imshow(heatmap.T, extent=extent, origin='lower', aspect='auto', cmap=cm.jet)
