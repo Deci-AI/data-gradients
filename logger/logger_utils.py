a04eb14ce435d690f0d6d666bdae034d1e998c40
tomer.keren <tomer.keren@deci.ai>
2022-12-11 12:50:08 +0200

    Added heatmap plot with gaussian filter

--- logger/logger_utils.py
+++ logger/logger_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, cm
+from scipy.ndimage import gaussian_filter
 
 
 def create_bar_plot(ax, data, labels, x_label: str = "", y_label: str = "", title: str = "",
@@ -21,3 +22,16 @@ def create_bar_plot(ax, data, labels, x_label: str = "", y_label: str = "", titl
     ax.set_ylabel(y_label)
     ax.set_title(title)
     ax.legend()
+
+
+def create_heatmap_plot(ax, x, y, bins=50, sigma=2, title="", x_label="", y_label="", gaussian_filter: bool=True):
+    heatmap, xedges, yedges = np.histogram2d(x, y, bins=bins)
+    if gaussian_filter:
+        heatmap = gaussian_filter(heatmap, sigma=sigma)
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    ax.imshow(heatmap.T, extent=extent, origin='lower', aspect='auto', cmap=cm.jet)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+
+
