9bcd75e1aac31629c2bcfd3dcf0d08aaafb5c3a9
tomer.keren <tomer.keren@deci.ai>
2022-12-14 09:54:30 +0200

    Added "if extent" to heatmap

--- logger/logger_utils.py
+++ logger/logger_utils.py
@@ -24,15 +24,18 @@ def create_bar_plot(ax, data, labels, x_label: str = "", y_label: str = "", titl
     ax.legend()
 
 
-def create_heatmap_plot(ax, x, y, train, bins=50, sigma=2, title="", x_label="", y_label="", use_gaussian_filter: bool=True):
-
+def create_heatmap_plot(ax, x, y, train, bins=50, sigma=2, title="", x_label="", y_label="", use_gaussian_filter: bool=True, use_extent: bool=True):
     heatmap, xedges, yedges = np.histogram2d(x, y, bins=bins)
     if use_gaussian_filter:
         heatmap = gaussian_filter(heatmap, sigma=sigma)
-    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-    ax.imshow(heatmap.T, extent=extent, origin='lower', aspect='auto', cmap=cm.jet)
+    if use_extent:
+        extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+        ax.imshow(heatmap.T, extent=extent, origin='lower', aspect='auto', cmap=cm.jet)
+    else:
+        ax.imshow(heatmap.T, origin='lower', aspect='auto', cmap=cm.jet)
+
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
-    ax.set_title("train" if train else "val" + " - " + title)
+    ax.set_title(("train" if train else "val") + " - " + title)
 
 
