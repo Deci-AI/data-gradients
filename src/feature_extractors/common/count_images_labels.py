cf1d46cc4c6f6a3602ac708843101d960b97323a
tomer.keren <tomer.keren@deci.ai>
2022-12-15 16:03:49 +0200

    Finished writing count images labels

--- src/feature_extractors/common/count_images_labels.py
+++ src/feature_extractors/common/count_images_labels.py
@@ -8,13 +8,18 @@ class NumberOfImagesLabels(FeatureExtractorAbstract):
         super().__init__()
         self._num_images: int = 0
         self._num_labels: int = 0
+        self._num_bg_images: int = 0
 
     def execute(self, data: BatchData):
         self._num_images += len(data.images)
         self._num_labels += len(data.labels)
+        for i, image_contours in enumerate(data.contours):
+            if len(image_contours) < 1:
+                self._num_bg_images += 1
 
     def process(self, ax, train):
-        # TODO: Add background images?
-        create_bar_plot(ax=ax, data=[self._num_images, self._num_labels],
-                        labels=["images", "labels"], y_label='Total #', title='# Images & Labels',
-                        train=train, ticks_rotation=0, color=self.colors[int(train)])
+
+        create_bar_plot(ax=ax, data=[self._num_images, self._num_labels, self._num_bg_images],
+                        labels=["images", "labels", "background images"], y_label='Total #', title='# Images & Labels',
+                        train=train, ticks_rotation=0, color=self.colors[int(train)],
+                        yticks=True)
