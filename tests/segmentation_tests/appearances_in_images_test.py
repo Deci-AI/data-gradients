c8997b5bf33600d2d904877f7f8cfcea30e8a250
tomer.keren <tomer.keren@deci.ai>
2022-12-19 16:37:49 +0200

    working on tests

---
+++ tests/segmentation_tests/appearances_in_images_test.py
@@ -0,0 +1,36 @@
+import unittest
+
+import torch
+
+from src.feature_extractors.segmentation.appearances_in_images import AppearancesInImages
+from src.preprocess import SegmentationPreprocessor
+from tests.segmentation_tests.example_dataset import train_loader, num_classes, ignore_labels
+
+
+class AppearancesInImagesTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self._feature_extractor = AppearancesInImages(num_classes=num_classes,
+                                                      ignore_labels=ignore_labels)
+        self._preprocessor = SegmentationPreprocessor(num_classes=num_classes,
+                                                      ignore_labels=ignore_labels)
+
+        self._train_dataloader = train_loader
+
+    def test(self):
+        images, labels = next(iter(self._train_dataloader))
+
+        segmentation_batch_data = self._preprocessor.preprocess(images, labels)
+
+        self._feature_extractor.execute(segmentation_batch_data)
+
+        all_unique_labels = torch.Tensor()
+        for label in labels:
+            unique = torch.unique(label)
+            all_unique_labels = torch.concat((all_unique_labels, unique[unique > 0]), dim=0)
+
+        for i in range(1, num_classes - 1):
+            assert torch.count_nonzero(all_unique_labels == i).item() == self._feature_extractor._hist[i], "Number of objects in the {i}th class"
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/segmentation_tests/squeeze_by_class_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
