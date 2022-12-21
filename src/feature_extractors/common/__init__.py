6154d96dd65f92bb5208440bc8d729f3920b7a29
tomer.keren <tomer.keren@deci.ai>
2022-12-21 10:37:28 +0200

    Removed redundant features

--- src/feature_extractors/common/__init__.py
+++ src/feature_extractors/common/__init__.py
@@ -3,5 +3,3 @@ from src.feature_extractors.common.average_brightness import *
 from src.feature_extractors.common.count_images_labels import *
 from src.feature_extractors.common.image_resolutions import *
 from src.feature_extractors.common.image_aspect_ratios import *
-from src.feature_extractors.common.number_background_images import *
-from src.feature_extractors.common.number_unique_classes import *
