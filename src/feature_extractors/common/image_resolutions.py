a3d20b7a6561b3538560979cb6d089a6f6c49a66
tomer.keren <tomer.keren@deci.ai>
2022-12-21 10:43:02 +0200

    Added image resolutions feature

--- src/feature_extractors/common/image_resolutions.py
+++ src/feature_extractors/common/image_resolutions.py
@@ -1,21 +1,24 @@
 from src.feature_extractors.feature_extractor_abstract import FeatureExtractorAbstract
+from src.logger.logger_utils import create_bar_plot
 
 
 class ImagesResolutions(FeatureExtractorAbstract):
     def __init__(self):
         super().__init__()
         self._res_dict = dict()
-        self._channels_last = False
 
     def execute(self, data):
         for image in data.images:
-            res = tuple(image.shape[:-1] if self._channels_last else image.shape[1:])
+            res = str(tuple(image.shape[1:]))
             if res not in self._res_dict:
-                self._res_dict[res] = 1
+                self._res_dict.update({res: 1})
             else:
                 self._res_dict[res] += 1
 
     def process(self, ax, train):
-        pass
-        # print('Resolutions dict: ', self._res_dict)
+        create_bar_plot(ax=ax, data=list(self._res_dict.values()), labels=list(self._res_dict.keys()),
+                        y_label='# Of Images', title='Image resolutions', x_label='Resolution [W, H]',
+                        train=train, ticks_rotation=0, color=self.colors[int(train)], yticks=True)
+        return self._res_dict
+
 
