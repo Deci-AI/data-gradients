612caffb26e340052a7cd3a8b9a3777e6a8d3f37
tomer.keren <tomer.keren@deci.ai>
2022-12-21 16:47:37 +0200

    internal use

--- internal_use_data_loaders/get_torch_loaders.py
+++ internal_use_data_loaders/get_torch_loaders.py
@@ -2,7 +2,7 @@ from typing import Tuple, Optional
 
 import torchvision.transforms
 from torchvision import datasets
-from torchvision.transforms import ToTensor, CenterCrop
+from torchvision.transforms import ToTensor, Resize
 from torch.utils.data import DataLoader
 
 from data.bdd_dataset import BDDDataset
@@ -10,16 +10,19 @@ from internal_use_data_loaders.cityscapes_dataset import CityScapesDataSet
 from internal_use_data_loaders.pp_humanseg_14k_dataset import PPHumanSegDataSet
 
 # TODO: Clean up all methods but "bdd"
+# TODO: Fix all transformations!!!
+# TODO: FUCK
 # Make dataset root path relative to project's, pointing at small example bdd dataset
 
 
 class DataLoaders:
-    def __init__(self):
-        self._transforms = torchvision.transforms.Compose([ToTensor(), CenterCrop(512)])
-        self._batch_size = 32
+    def __init__(self, batch_size: int = 16):
+        self._transforms = torchvision.transforms.Compose([ToTensor(), Resize(size=256)])
+        self._target_transforms = torchvision.transforms.Compose([ToTensor()])
+        self._batch_size = batch_size
 
     def _dataset_to_dataloader(self, dataset):
-        return DataLoader(dataset, batch_size=self._batch_size, shuffle=True)
+        return DataLoader(dataset, batch_size=self._batch_size, shuffle=True, num_workers=0)
 
     def bdd(self):
         dataset_root = "/Users/tomerkeren/workspace/deci-dataset-analyzer/data/bdd100k"
@@ -36,7 +39,6 @@ class DataLoaders:
             image_set="train",
             mode="segmentation",
             download=False,
-            transforms=self._transforms
         )
 
         val = datasets.SBDataset(
@@ -44,7 +46,6 @@ class DataLoaders:
             image_set="val",
             mode="segmentation",
             download=False,
-            transforms=self._transforms
         )
 
         train_dataloader = self._dataset_to_dataloader(train)
@@ -55,11 +56,13 @@ class DataLoaders:
         dataset_root = "/Users/tomerkeren/workspace/deci-dataset-analyzer/data/pp_humanseg/PP-HumanSeg14K"
         train = PPHumanSegDataSet(root=dataset_root,
                                   image_set='train',
-                                  transform=self._transforms)
+                                  transform=self._transforms,
+                                  target_transform=self._target_transforms)
 
         val = PPHumanSegDataSet(root=dataset_root,
                                 image_set='val',
-                                transform=self._transforms)
+                                transform=self._transforms,
+                                target_transform=self._target_transforms)
         train_dataloader = self._dataset_to_dataloader(train)
         val_dataloader = self._dataset_to_dataloader(val)
         return train_dataloader, val_dataloader
@@ -68,12 +71,16 @@ class DataLoaders:
         dataset_root = "/Users/tomerkeren/workspace/deci-dataset-analyzer/data/cityscapes"
         train = CityScapesDataSet(root=dataset_root,
                                   image_set='train',
-                                  transform=self._transforms)
+                                  transform=self._transforms,
+                                  target_transform=self._target_transforms)
         val = CityScapesDataSet(root=dataset_root,
                                 image_set='val',
-                                transform=self._transforms)
+                                transform=self._transforms,
+                                target_transform=self._target_transforms)
+
         train_dataloader = self._dataset_to_dataloader(train)
         val_dataloader = self._dataset_to_dataloader(val)
+
         return train_dataloader, val_dataloader
 
     def get_dataloader(self, dataset: str) -> Tuple[DataLoader, Optional[DataLoader]]:
@@ -82,11 +89,6 @@ class DataLoaders:
 
         elif dataset == 'bdd':
             return self.bdd()
-        elif dataset == "kitti":
-            raise NotImplementedError
-
-        elif dataset == "voc_seg":
-            raise NotImplementedError
 
         elif dataset == "pp_human":
             return self.pp_human()
@@ -97,14 +99,12 @@ class DataLoaders:
             raise NotImplementedError
 
 
-sbd = True
-kitti = False
-voc_segmentation = False
 # [0, 1, 2, 3, 4, 5, 6, 9, 10, 14, 15, 16, 18, 29, 30, -1]
-# sbd, pp_human, cityscapes, bdd
-# 20,  1,        19,         40
-train_dataloader, val_dataloader = DataLoaders().get_dataloader(dataset="bdd")
-train_data_iterator, val_data_iterator = iter(train_dataloader), iter(val_dataloader)
-num_classes = 20
-ignore_labels = [0]
+# pp_human, cityscapes, bdd, sbd,
+# 1,        19,         19,  ?
 
+dataloader = DataLoaders(batch_size=16)
+train_dataloader, val_dataloader = dataloader.get_dataloader(dataset="bdd")
+train_data_iterator, val_data_iterator = iter(train_dataloader), iter(val_dataloader)
+num_classes = 19
+ignore_labels = [0] #  [0, 1, 2, 3, 4, 5, 6, 9, 10, 14, 15, 16, 18, 29, 30, -1]
