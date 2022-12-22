a05e33f2cd90df586081b4c0a0a30e5e677eba55
tomer.keren <tomer.keren@deci.ai>
2022-12-22 17:34:50 +0200

    Fixed logdir bug

--- src/logger/results_logger.py
+++ src/logger/results_logger.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
 import datetime as dt
 
+log_dir = 'logs/' + dt.datetime.now().strftime("%Y%m%d-%H%M%S")
+
 
 class ResultsLogger(ABC):
     def __init__(self):
-        self.logdir = "logs/" + dt.datetime.now().strftime("%Y%m%d-%H%M%S")
+        self.logdir = log_dir
 
     @abstractmethod
     def log(self, title: str, data):
