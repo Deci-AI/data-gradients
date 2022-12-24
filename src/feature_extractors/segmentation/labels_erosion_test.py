39996105516bf6f6242e4646dedf1b81f2fcda19
tomer.keren <tomer.keren@deci.ai>
2022-12-24 17:58:44 +0200

    Created base-features for two new features

---
+++ src/feature_extractors/segmentation/labels_erosion_test.py
@@ -0,0 +1,19 @@
+from src.utils import SegBatchData
+from src.feature_extractors.segmentation.segmentation_abstract import SegmentationFeatureExtractorAbstract
+from src.logger.logger_utils import create_bar_plot
+
+
+class ErosionTest(SegmentationFeatureExtractorAbstract):
+    """
+    Semantic Segmentation task feature extractor -
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def execute(self, data: SegBatchData):
+        for image_contours in data.contours:
+            pass
+
+    def process(self, ax, train):
+        pass
