d10d2a9e025baeda8796305af104ecd3e39f4fb1
tomer.keren <tomer.keren@deci.ai>
2022-12-25 16:36:22 +0200

    added temporary debug function for convexity program

--- src/preprocess/contours.py
+++ src/preprocess/contours.py
@@ -4,6 +4,69 @@ import cv2.cv2 as cv2
 import numpy as np
 import torch
 
+def debug_convexity_things(labels, images):
+    for label, image in zip(labels, images):
+        drawing = image.numpy()
+        drawing = drawing.transpose(1, 2, 0)
+        drawing = cv2.cvtColor(drawing, cv2.COLOR_RGB2BGR)
+        cv2.imshow("original", drawing)
+
+        label = label.numpy().astype(np.uint8)
+        for class_channel in range(label.shape[0]):
+            onehot = label[class_channel, ...]
+            convex_hull = []
+            bbox = []
+            onehot_contours, hierarchy = cv2.findContours(onehot, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            for i in range(len(onehot_contours)):
+                convex_hull.append(get_convex_hull(onehot_contours[i]))
+                bbox.append(rect_to_box(get_rotated_bounding_rect(onehot_contours[i])))
+
+            for i in range(len(onehot_contours)):
+                img_contours = np.zeros_like(drawing)
+                cv2.drawContours(img_contours, onehot_contours, i, (0, 255, 0), 1, 8, hierarchy)
+                cv2.putText(img_contours, "Contour", (20, 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+                cv2.drawContours(img_contours, convex_hull, i, (0, 0, 255), 1, 8)
+                cv2.putText(img_contours, "Convex hull", (20, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+                cv2.drawContours(img_contours, bbox, i, (255, 0, 0), 1, 8)
+                cv2.putText(img_contours, "Bbox", (20, 60), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+                contour_area = round(get_contour_area(onehot_contours[i]))
+                hull_area = round(get_contour_area(convex_hull[i]))
+                if not hull_area > 0:
+                    print(convex_hull[i], onehot_contours[i], contour_area, hull_area)
+                    continue
+                ratio = float(round(contour_area / hull_area, 3))
+                cv2.putText(img_contours, f"Contour area - {contour_area}", (20, 80), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+                cv2.putText(img_contours, f"HULL area - {hull_area}", (20, 100), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+                cv2.putText(img_contours, f'Ratio - {ratio}', (20, 120), cv2.FONT_HERSHEY_PLAIN, 1,
+                            (255, 255, 255), 1)
+
+                hull = cv2.convexHull(onehot_contours[i], returnPoints=False)
+                defects = cv2.convexityDefects(onehot_contours[i], hull)
+                for defect in defects:
+                    defect = defect[0]
+                    startpoint = onehot_contours[i][defect[0]][0]
+                    endpoint = onehot_contours[i][defect[1]][0]
+                    farthestpoint = onehot_contours[i][defect[2]][0]
+                    distance = defect[3]
+                    if distance > 5000:
+                        # cv2.line(img_contours, endpoint, startpoint, (255, 0, 255), 1)
+                        print(endpoint, startpoint)
+                        mid = [int(abs((endpoint[0] + startpoint[0])/2)), int(abs((endpoint[1] + startpoint[1]))/2)]
+                        print(mid)
+                        cv2.circle(img_contours, startpoint, 2, (255, 0, 255), 2)
+                        cv2.circle(img_contours, endpoint, 2, (255, 0, 255), 2)
+                        cv2.circle(img_contours, mid, 2, (255, 0, 255), 2)
+                        cv2.line(img_contours, mid, farthestpoint, (0, 255, 255), 1)
+                        cv2.putText(img_contours, f"{str(distance)}", mid, cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255))
+                    cv2.imshow(f"contouyr", img_contours)
+                    cv2.waitKey(0)
+def get_box_area(box):
+    print(box)
+    x1, y1 = box[0]
+    x2, y2 = box[1]
+    print(abs(y2-y1), abs(x2-x1))
+    area = abs(y2-y1) * abs(x2-x1)
+    return area
 
 def get_contours(label: torch.Tensor) -> np.array:
     """
@@ -78,6 +141,11 @@ def get_contour_perimeter(contour: np.array) -> float:
 
 def get_contour_is_convex(contour: np.array) -> bool:
     return cv2.isContourConvex(contour)
+
+
+def get_convex_hull(contour: np.array) -> np.array:
+    convex_hull = cv2.convexHull(contour, hull=False)
+    return convex_hull
 
 
 def get_rotated_bounding_rect(contour: np.array) -> np.array:
