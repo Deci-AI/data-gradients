de2ce5655a57127d3d54c00917ebd733748e37fe
tomer.keren <tomer.keren@deci.ai>
2022-12-27 15:03:27 +0200

    Protective bins + better rounding

--- src/logger/logger_utils.py
+++ src/logger/logger_utils.py
@@ -25,7 +25,7 @@ def create_bar_plot(ax, data, labels, split: str, x_label: str = "", y_label: st
 
     if yticks:
         for i in range(len(labels)):
-            v = np.round(data[i], 1) if np.round(data[i], 1) > 0. else ""
+            v = np.round(data[i], 2) if np.round(data[i], 2) > 0. else ""
             plt.text(x=i - (width / 2 if (split == 'train') else -width / 2),
                      y=data[i] + 0.75,
                      s=v,
@@ -39,6 +39,8 @@ def create_bar_plot(ax, data, labels, split: str, x_label: str = "", y_label: st
 
 
 def create_heatmap_plot(ax, x, y, split: str, bins=50, sigma=2, title="", x_label="", y_label="", use_gaussian_filter: bool=True, use_extent: bool=True):
+    if bins == 0:
+        bins = 1
     heatmap, xedges, yedges = np.histogram2d(x, y, bins=bins)
     if use_gaussian_filter:
         heatmap = gaussian_filter(heatmap, sigma=sigma)
