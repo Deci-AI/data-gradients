d8b17e4569475a318c9f32e72d7af815bead697d
tomer.keren <tomer.keren@deci.ai>
2022-12-27 15:02:35 +0200

    W/H instead of H/W

--- src/feature_extractors/common/image_aspect_ratios.py
+++ src/feature_extractors/common/image_aspect_ratios.py
@@ -7,22 +7,22 @@ from src.logger.logger_utils import create_bar_plot
 class ImagesAspectRatios(FeatureExtractorAbstract):
     def __init__(self):
         super().__init__()
-        self._ar_dict = {'train': dict(), 'val': dict()}
+        self._hist = {'train': dict(), 'val': dict()}
 
     def execute(self, data):
         for image in data.images:
-            ar = np.round(image.shape[1] / image.shape[2], 2)
-            if ar not in self._ar_dict[data.split]:
-                self._ar_dict[data.split][ar] = 1
+            ar = np.round(image.shape[2] / image.shape[1], 2)
+            if ar not in self._hist[data.split]:
+                self._hist[data.split][ar] = 1
             else:
-                self._ar_dict[data.split][ar] += 1
+                self._hist[data.split][ar] += 1
 
     def _process(self):
         self.merge_dict_splits(self._hist)
 
         for split in ['train', 'val']:
-            create_bar_plot(ax=self.ax, data=list(self._ar_dict[split].values()),
-                            labels=list(self._ar_dict[split].keys()), y_label='# Of Images',
+            create_bar_plot(ax=self.ax, data=list(self._hist[split].values()),
+                            labels=list(self._hist[split].keys()), y_label='# Of Images',
                             title='Image aspect ratios', x_label='Aspect ratio [W / H]', split=split, ticks_rotation=0,
                             color=self.colors[split], yticks=True)
-            self.json_object.update({split: self._ar_dict[split]})
+            self.json_object.update({split: self._hist[split]})
