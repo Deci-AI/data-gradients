c2d2e5b5f8dae99a65b4219445739e62ea3a0535
tomer.keren <tomer.keren@deci.ai>
2023-01-05 11:54:06 +0200

    output file out of state

--- src/logging/json_logger.py
+++ src/logging/json_logger.py
@@ -1,22 +1,21 @@
 import os
 import json
-from typing import Dict
 
 from src.logging.results_logger import ResultsLogger
 
 
 class JsonLogger(ResultsLogger):
 
-    def __init__(self):
+    def __init__(self, output_file_name: str):
         super().__init__()
-        self._output_file = 'raw_data.json'
         self._logging_data = {}
+        self.output_file_name = output_file_name + 'json'
 
     def log(self, title: str, data):
         self._logging_data.update({title: data})
 
     def write_to_json(self):
-        with open(os.path.join(self.logdir, self._output_file), 'a') as output:
+        with open(os.path.join(self.logdir, self.output_file_name), 'a') as output:
             try:
                 json.dump(self._logging_data, output, indent=4)
             except Exception as e:
