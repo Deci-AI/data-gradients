53854a7793dd4e57a71b3268ed681e54c83bdf3f
tomer.keren <tomer.keren@deci.ai>
2023-01-09 20:34:05 +0200

    try except visualizations until i figure it out

--- src/data_gradients/logging/tensorboard_logger.py
+++ src/data_gradients/logging/tensorboard_logger.py
@@ -17,22 +17,26 @@ class TensorBoardLogger(ResultsLogger):
         if self.samples_to_visualize == 0:
             return
         n = 0
-        while n < self.samples_to_visualize:
-            # TODO: Still WIP
-            images, labels = next(self._train_iter)
-            if len(images) > self.samples_to_visualize:
-                images = images[:self.samples_to_visualize]
-                labels = labels[:self.samples_to_visualize]
+        try:
+            while n < self.samples_to_visualize:
 
-            labels *= (1. / max(labels.unique()))
-            labels = labels.repeat(1, 3, 1, 1)
+                # TODO: Still WIP
+                images, labels = next(self._train_iter)
+                if len(images) > self.samples_to_visualize:
+                    images = images[:self.samples_to_visualize]
+                    labels = labels[:self.samples_to_visualize]
 
-            img_grid = torchvision.utils.make_grid(torch.cat([images, labels]), nrow=len(images))
+                labels *= (1. / max(labels.unique()))
+                labels = labels.repeat(1, 3, 1, 1)
 
-            n += len(images)
+                img_grid = torchvision.utils.make_grid(torch.cat([images, labels]), nrow=len(images))
 
-            # write to tensorboard
-            self.writer.add_image(f'{n}_images_labels', img_grid)
+                n += len(images)
+
+                # write to tensorboard
+                self.writer.add_image(f'{n}_images_labels', img_grid)
+        except Exception as e:
+            print(f'\nCould not visualize images on tensorboard\n{e}')
 
     def log(self, title, data):
         title += "/fig"
