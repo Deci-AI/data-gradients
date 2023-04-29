a3e51c5a66d051ff2a010755877e6b20723e1edb
Louis Dupont <louis-dupont@live.fr>
2023-04-29 21:13:49 +0300

    add docstrings

--- src/data_gradients/batch_processors/base.py
+++ src/data_gradients/batch_processors/base.py
@@ -8,7 +8,17 @@ from data_gradients.batch_processors.validators.base import BatchValidator
 
 
 class BatchProcessor(ABC):
+    """A callable abstract base class responsible for processing raw batches (coming from a DataLoader) into
+    ready-to-analyze batch objects. It handles extraction, validation, and preprocessing of the images and labels.
+
+    """
+
     def __init__(self, batch_extractor: BatchExtractor, batch_validator: BatchValidator, batch_preprocessor: BatchPreprocessor):
+        """
+        :param batch_extractor:     Object responsible for extracting images and labels from the raw batch.
+        :param batch_validator:     Object responsible for validating the format of images and labels.
+        :param batch_preprocessor:  Object responsible for preprocessing images and labels into a ready-to-analyze batch object.
+        """
         self.batch_extractor = batch_extractor
         self.batch_validator = batch_validator
         self.batch_preprocessor = batch_preprocessor
