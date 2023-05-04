b959fa0f885259645bb1d08903649ab7a7005a53
shayaharon <shay.aharon@deci.ai>
2023-05-04 15:48:49 +0300

    wip

--- src/data_gradients/common/factories/feature_extractors_factory.py
+++ src/data_gradients/common/factories/feature_extractors_factory.py
@@ -2,7 +2,7 @@ from .base_factory import BaseFactory
 from ..registry.registry import FEATURE_EXTRACTORS
 
 
-class CallbacksFactory(BaseFactory):
+class FeatureExtractorsFactory(BaseFactory):
     def __init__(self):
         super().__init__(FEATURE_EXTRACTORS)
 
