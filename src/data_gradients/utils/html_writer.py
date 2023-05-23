78c277b34b66c2f88613ab996fd01862fc77627a
ofri masad <ofri@deci.ai>
2023-05-23 10:35:30 +0300

    html and pdf writer

--- src/data_gradients/utils/html_writer.py
+++ src/data_gradients/utils/html_writer.py
@@ -1,28 +1,87 @@
+from dataclasses import dataclass
+
+from xhtml2pdf import pisa
 
 from data_gradients.utils.common.assets_container import assets
 
-from xhtml2pdf import pisa             # import python module
+FEATURE_NAME_KEY = "{{feature_name}}"
+FEATURE_DESCRIPTION_KEY = "{{feature_description}}"
+FEATURE_IMAGE_PATH_KEY = "{{feature_image_path}}"
+SECTION_NAME_KEY = "{{section_name}}"
+FEATURES_KEY = "{{features}}"
+SECTIONS_KEY = "{{sections}}"
+TITLE_KEY = "{{title}}"
+SUBTITLE_KEY = "{{subtitle}}"
+LOGO_PATH_KEY = "{{logo_path}}"
 
 
-# Utility function
-def convert_html_to_pdf(source_html, output_filename):
-    # open output file for writing (truncated binary)
-    result_file = open(output_filename, "w+b")
+@dataclass
+class Feature:
+    name: str
+    description: str
+    image_path: str
 
-    # convert HTML to PDF
-    pisa_status = pisa.CreatePDF(
-            source_html,                # the HTML to convert
-            dest=result_file)           # file handle to recieve result
 
-    # close output file
-    result_file.close()                 # close output file
+class Section:
+    def __init__(self, section_name):
+        self.section_name = section_name
+        self.features = []
 
-    # return False on success and True on errors
-    return pisa_status.err
+    def add_feature(self, feature: Feature):
+        self.features.append(feature)
 
-# Main program
-if __name__ == "__main__":
-    pisa.showLogging()
-    html = assets.html.base_template
-    html = html.replace("image1.png", assets.image.image1)
-    convert_html_to_pdf(html, './out.pdf')
+
+class ResultsContainer:
+
+    def __init__(self):
+        self.sections = []
+
+    def add_section(self, section: Section):
+        self.sections.append(section)
+
+
+class HtmlWriter:
+
+    def __init__(self, title: str, subtitle: str, doc_template: str = assets.html.doc_template, section_template: str = assets.html.section_template,
+                 feature_template: str = assets.html.feature_template, logo_path: str = assets.image.logo):
+        self.title = title
+        self.subtitle = subtitle
+        self.doc_template = doc_template
+        self.section_template = section_template
+        self.feature_template = feature_template
+        self.logo_path = logo_path
+
+    def get_html(self, results_container: ResultsContainer):
+        sections_html = ""
+        for section_index, section in enumerate(results_container.sections):
+
+            section_name = f"{section_index + 1}.\t{section.section_name}"
+            features = section.features
+            features_html = ""
+            for feature_index, feature in enumerate(features):
+                feature_name = f"{section_index + 1}.{feature_index + 1}.\t{feature.name}"
+                feature_description = feature.description
+                feature_image_path = feature.image_path
+
+                features_html += self.feature_template.replace(FEATURE_NAME_KEY, feature_name) \
+                    .replace(FEATURE_DESCRIPTION_KEY, feature_description) \
+                    .replace(FEATURE_IMAGE_PATH_KEY, feature_image_path)
+
+            sections_html += self.section_template.replace(SECTION_NAME_KEY, section_name) \
+                .replace(FEATURES_KEY, features_html)
+
+        doc = self.doc_template.replace(SECTIONS_KEY, sections_html).replace(TITLE_KEY, self.title) \
+            .replace(SUBTITLE_KEY, self.subtitle).replace(LOGO_PATH_KEY, self.logo_path)
+        return doc
+
+    def write(self, results_container: ResultsContainer, output_filename: str):
+        assert output_filename.endswith("html")
+        doc = self.get_html(results_container)
+        with open(output_filename, "w") as result_file:
+            result_file.write(doc)
+
+    def write_PDF(self, results_container: ResultsContainer, output_filename: str):
+        assert output_filename.endswith("pdf")
+        doc = self.get_html(results_container)
+        with open(output_filename, "w+b") as result_file:
+            pisa.CreatePDF(doc, dest=result_file)
---
+++ tests/unit_tests/html_pdf_writer_tests.py
@@ -0,0 +1,28 @@
+import os
+import unittest
+
+from data_gradients.utils.common.assets_container import assets
+from data_gradients.utils.html_writer import ResultsContainer, Section, Feature, HtmlWriter
+
+
+class VisualizationTests(unittest.TestCase):
+    """ """
+
+    def setUp(self):
+        self.results_c = ResultsContainer()
+
+        for section_index in range(4):
+            section = Section(f"Section {section_index}")
+            for feature_index in range(6):
+                section.add_feature(Feature(f"Feature {feature_index}", assets.text.lorem_ipsum, assets.image.chart_demo))
+            self.results_c.add_section(section)
+
+    def test_pdf_generation(self):
+
+        html_writer = HtmlWriter(title="Data Gradients", subtitle="test 1", doc_template=assets.html.doc_template,
+                                 section_template=assets.html.section_template, feature_template=assets.html.feature_template)
+        html_writer.write_PDF(self.results_c, "./out.pdf")
+        self.assertTrue(os.path.exists("./out.pdf"))
+
+if __name__ == "__main__":
+    unittest.main()
