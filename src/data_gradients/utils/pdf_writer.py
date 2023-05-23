aad88e29adb6bb90f18e1cef04e7283f05d477e7
ofri masad <ofri@deci.ai>
2023-05-23 22:52:25 +0300

    fix according to review and add docs

--- src/data_gradients/utils/pdf_writer.py
+++ src/data_gradients/utils/pdf_writer.py
@@ -32,7 +32,10 @@ class Section:
 
 
 class ResultsContainer:
-
+    """
+    A container for the results of the analysis.
+    dived to sections and features.
+    """
     def __init__(self):
         self.sections = []
 
@@ -40,10 +43,23 @@ class ResultsContainer:
         self.sections.append(section)
 
 
-class HtmlWriter:
+class PDFWriter:
+    """
+    This class is responsible for generating the PDF file.
+    It uses the pisa library to generate the PDF file.
 
+    The PDF file is generated based on HTML templates (document, section and feature templates).
+    """
     def __init__(self, title: str, subtitle: str, doc_template: str = assets.html.doc_template, section_template: str = assets.html.section_template,
                  feature_template: str = assets.html.feature_template, logo_path: str = assets.image.logo):
+        """
+        :param title: The title of the PDF document.
+        :param subtitle: The subtitle of the PDF document.
+        :param doc_template: The path to the document template.
+        :param section_template: The path to the section template.
+        :param feature_template: The path to the feature template.
+        :param logo_path: The path to the logo image.
+        """
         self.title = title
         self.subtitle = subtitle
         self.doc_template = doc_template
@@ -51,7 +67,7 @@ class HtmlWriter:
         self.feature_template = feature_template
         self.logo_path = logo_path
 
-    def get_html(self, results_container: ResultsContainer):
+    def _get_html(self, results_container: ResultsContainer):
         sections_html = ""
         for section_index, section in enumerate(results_container.sections):
 
@@ -75,13 +91,13 @@ class HtmlWriter:
         return doc
 
     def write(self, results_container: ResultsContainer, output_filename: str):
-        assert output_filename.endswith("html")
-        doc = self.get_html(results_container)
-        with open(output_filename, "w") as result_file:
-            result_file.write(doc)
+        """
+        :param results_container: The results container containing the sections and features.
+        :param output_filename: The path to the output file.
+        """
+        if not output_filename.endswith("pdf"):
+            raise RuntimeError("filename must end with .pdf")
 
-    def write_PDF(self, results_container: ResultsContainer, output_filename: str):
-        assert output_filename.endswith("pdf")
-        doc = self.get_html(results_container)
+        doc = self._get_html(results_container)
         with open(output_filename, "w+b") as result_file:
             pisa.CreatePDF(doc, dest=result_file)
--- tests/unit_tests/pdf_writer_tests.py
+++ tests/unit_tests/pdf_writer_tests.py
@@ -2,11 +2,10 @@ import os
 import unittest
 
 from data_gradients.utils.common.assets_container import assets
-from data_gradients.utils.html_writer import ResultsContainer, Section, Feature, HtmlWriter
+from data_gradients.utils.pdf_writer import ResultsContainer, Section, Feature, PDFWriter
 
 
-class HTML_PDF_Writer_Test(unittest.TestCase):
-    """ """
+class PDF_Writer_Test(unittest.TestCase):
 
     def setUp(self):
         self.results_c = ResultsContainer()
@@ -19,10 +18,13 @@ class HTML_PDF_Writer_Test(unittest.TestCase):
 
     def test_pdf_generation(self):
 
-        html_writer = HtmlWriter(title="Data Gradients", subtitle="test 1", doc_template=assets.html.doc_template,
-                                 section_template=assets.html.section_template, feature_template=assets.html.feature_template)
-        html_writer.write_PDF(self.results_c, "./out.pdf")
+        html_writer = PDFWriter(title="Data Gradients", subtitle="test 1", doc_template=assets.html.doc_template,
+                                section_template=assets.html.section_template, feature_template=assets.html.feature_template)
+        html_writer.write(self.results_c, "./out.pdf")
         self.assertTrue(os.path.exists("./out.pdf"))
+
+    def tearDown(self):
+        os.remove("./out.pdf")
 
 
 if __name__ == "__main__":
