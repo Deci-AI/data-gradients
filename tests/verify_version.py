917840331da32e146163ce81c481d0eb60bba8fc
Shay Aharon <80472096+shaydeci@users.noreply.github.com>
2023-05-28 17:10:32 +0300

    added version (#43)

---
+++ tests/verify_version.py
@@ -0,0 +1,12 @@
+import sys
+
+import data_gradients
+
+if __name__ == "__main__":
+
+    ci_version = sys.argv[1]
+    if ci_version == data_gradients.__version__:
+        sys.exit(0)
+    else:
+        print(f"wrong version definition:\nCI version: {ci_version}\nsuper_gradients.__version__: {super_gradients.__version__}")
+        sys.exit(1)
