6f14d1b0bb1632c31ccdfee19953405b8df79789
Louis-Dupont <35190946+Louis-Dupont@users.noreply.github.com>
2023-06-01 12:02:02 +0300

    Feature/sg 889 add main detection features (#47)
    * working version
    
    * remove
    
    * working version
    
    * separate bbox and labels
    
    * remove padded values
    
    * add
    
    * add
    
    * wip
    
    * fix format
    
    * add typing
    
    * add doc
    
    * rename
    
    * remove __all__
    
    * add
    
    * add doc
    
    * add violinplot
    
    * fix
    
    * add first new feature as an example
    
    * fix
    
    * fix
    
    * improve example
    
    * improve example
    
    * use ask_user in batch adapter
    
    * fix
    
    * add some explanation to example
    
    * undo dataset change
    
    * add property
    
    * give option to set bandwidth and add json
    
    * cleanup
    
    * wip
    
    * add aspect_ratio
    
    * add new features
    
    * merge updates
    
    * update typing
    
    * fix
    
    * add WIP
    
    * undo unwanted change
    
    * fix descriptions
    
    * add ImageColorDistribution
    
    * rename classes
    
    * rename file
    
    * cast as uint8
    
    * add new features
    
    * remove unwanted change
    
    * update
    
    * fix
    
    * merge dete
    
    * add main detection features
    
    * add convexity and erosion
    
    * wip
    
    * fix erosion
    
    * add xlim to violinplot
    
    * fix
    
    * fix plots
    
    * add stat docstirng
    
    * minor doc
    
    * check unique image format
    
    * fix
    
    * fix
    
    * fix doc
    
    * renaming label to class_id
    
    * fix typo
    
    ---------
    
    Co-authored-by: Ran Rubin <ranrubin@gmail.com>
    Co-authored-by: Eugene Khvedchenya <ekhvedchenya@gmail.com>

--- src/data_gradients/feature_extractors/object_detection/bounding_boxes_area.py
+++ src/data_gradients/feature_extractors/object_detection/bounding_boxes_area.py
@@ -2,51 +2,45 @@ import pandas as pd
 
 from data_gradients.common.registry.registry import register_feature_extractor
 from data_gradients.feature_extractors.feature_extractor_abstractV2 import Feature
-from data_gradients.utils.data_classes import SegmentationSample
+from data_gradients.utils.data_classes import DetectionSample
 from data_gradients.visualize.seaborn_renderer import ViolinPlotOptions
 from data_gradients.feature_extractors.feature_extractor_abstractV2 import AbstractFeatureExtractor
 
 
 @register_feature_extractor()
-class SegmentationBoundingBoxArea(AbstractFeatureExtractor):
-    """
-    Semantic Segmentation task feature extractor -
-    Get all Bounding Boxes areas and plot them as a percentage of the whole image.
-    """
+class DetectionBoundingBoxArea(AbstractFeatureExtractor):
+    """Feature Extractor to compute the area covered Bounding Boxes."""
 
     def __init__(self):
         self.data = []
 
-    def update(self, sample: SegmentationSample):
+    def update(self, sample: DetectionSample):
         image_area = sample.image.shape[0] * sample.image.shape[1]
-        for class_channel in sample.contours:
-            for contour in class_channel:
-                class_id = contour.class_id
-                class_name = str(class_id) if sample.class_names is None else sample.class_names[class_id]
-                self.data.append(
-                    {
-                        "split": sample.split,
-                        "class_name": class_name,
-                        "bbox_area": 100 * (contour.bbox_area / image_area),
-                    }
-                )
+        for class_id, bbox_xyxy in zip(sample.class_ids, sample.bboxes_xyxy):
+            class_name = str(class_id) if sample.class_names is None else sample.class_names[class_id]
+            bbox_area = (bbox_xyxy[2] - bbox_xyxy[0]) * (bbox_xyxy[3] - bbox_xyxy[1])
+            self.data.append(
+                {
+                    "split": sample.split,
+                    "class_name": class_name,
+                    "relative_bbox_area": 100 * (bbox_area / image_area),
+                }
+            )
 
     def aggregate(self) -> Feature:
         df = pd.DataFrame(self.data)
 
-        max_area = min(100, df["bbox_area"].max())
         plot_options = ViolinPlotOptions(
-            x_label_key="bbox_area",
+            x_label_key="relative_bbox_area",
             x_label_name="Bounding Box Area (in % of image)",
             y_label_key="class_name",
             y_label_name="Class",
             title=self.title,
-            x_lim=(0, max_area),
             x_ticks_rotation=None,
             labels_key="split",
             bandwidth=0.4,
         )
-        json = dict(df.bbox_area.describe())
+        json = dict(df["relative_bbox_area"].describe())
 
         feature = Feature(
             data=df,
@@ -62,7 +56,7 @@ class SegmentationBoundingBoxArea(AbstractFeatureExtractor):
     @property
     def description(self) -> str:
         return (
-            "The distribution of the areas of the boxes that bound connected components of the different classes as a histogram.\n"
+            "The distribution of the areas of the boxes of the different classes.\n"
             "The size of the objects can significantly affect the performance of your model. "
             "If certain classes tend to have smaller objects, the model might struggle to segment them, especially if the resolution of the images is low "
         )
---
+++ tests/manual_tests/detection_super_gradients_test.py
@@ -0,0 +1,88 @@
+from tqdm import tqdm
+
+from data_gradients.batch_processors.detection import DetectionBatchProcessor
+
+from data_gradients.feature_extractors.commonV2.image_resolution import ImagesResolution
+from data_gradients.feature_extractors.commonV2.image_average_brightness import ImagesAverageBrightness
+from data_gradients.feature_extractors.commonV2.image_color_distribution import ImageColorDistribution
+from data_gradients.feature_extractors.object_detection.bounding_boxes_area import DetectionBoundingBoxArea
+from data_gradients.feature_extractors.object_detection.bounding_boxes_resolution import DetectionBoundingBoxSize
+from data_gradients.feature_extractors.object_detection.classes_count import DetectionClassesCount
+from data_gradients.feature_extractors.object_detection.classes_per_image_count import DetectionClassesPerImageCount
+from data_gradients.feature_extractors.object_detection.bounding_boxes_per_image_count import DetectionBoundingBoxPerImageCount
+
+from data_gradients.visualize.seaborn_renderer import SeabornRenderer
+import numpy as np
+from torch.utils.data import DataLoader
+
+# Note: This example will require you to install the super-gradients package
+from super_gradients.training.datasets import YoloDarknetFormatDetectionDataset
+
+
+class PadTarget:
+    """Transform targets (Compatible with Sg DetectionDatasets)"""
+
+    def __init__(self, max_targets: int):
+        self.max_targets = max_targets
+
+    def __call__(self, sample):
+        targets = sample["target"]
+        targets = np.ascontiguousarray(targets, dtype=np.float32)
+        padded_targets = np.zeros((self.max_targets, targets.shape[-1]))
+        padded_targets[range(len(targets))[: self.max_targets]] = targets[: self.max_targets]
+        padded_targets = np.ascontiguousarray(padded_targets, dtype=np.float32)
+        sample["target"] = padded_targets
+        return sample
+
+
+data_dir = "/Users/Louis.Dupont/Downloads/avatar_recognition.v2-release.yolov8"
+classes = ["Character"]
+
+# Create torch DataSet
+train_dataset = YoloDarknetFormatDetectionDataset(
+    data_dir=data_dir,
+    images_dir="train/images",
+    labels_dir="train/labels",
+    classes=classes,
+    transforms=[PadTarget(max_targets=50)],
+)
+val_dataset = YoloDarknetFormatDetectionDataset(
+    data_dir=data_dir,
+    images_dir="valid/images",
+    labels_dir="valid/labels",
+    classes=classes,
+    transforms=[PadTarget(max_targets=50)],
+)
+
+# Create torch DataLoader
+train_loader = DataLoader(train_dataset, batch_size=8)
+val_loader = DataLoader(val_dataset, batch_size=8)
+batch_processor = DetectionBatchProcessor(n_image_channels=3)
+
+feature_extractors = [
+    ImagesResolution(),
+    ImagesAverageBrightness(),
+    ImageColorDistribution(),
+    DetectionBoundingBoxPerImageCount(),
+    DetectionBoundingBoxArea(),
+    DetectionBoundingBoxSize(),
+    DetectionClassesCount(),
+    DetectionClassesPerImageCount(),
+]
+
+sns = SeabornRenderer()
+
+for train_batch in tqdm(train_loader):
+    for sample in batch_processor.process(train_batch, split="train"):
+        for feature_extractor in feature_extractors:
+            feature_extractor.update(sample)
+
+for val_batch in tqdm(val_loader):
+    for sample in batch_processor.process(val_batch, split="valid"):
+        for feature_extractor in feature_extractors:
+            feature_extractor.update(sample)
+
+for feature_extractor in feature_extractors:
+    feature = feature_extractor.aggregate()
+    f = sns.render(feature.data, feature.plot_options)
+    f.show()
