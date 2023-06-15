1ecc4fe7a7961875c5d8bf77a4ca61cf83ac699d
Ofri Masad <ofrimasad@users.noreply.github.com>
2023-06-15 12:02:07 +0300

    minor format fixes (#95)
    * minor format fixes
    
    * cleanup

--- src/data_gradients/feature_extractors/object_detection/__init__.py
+++ src/data_gradients/feature_extractors/object_detection/__init__.py
@@ -1,7 +1,7 @@
 from .bounding_boxes_area import DetectionBoundingBoxArea
 from .bounding_boxes_per_image_count import DetectionBoundingBoxPerImageCount
 from .bounding_boxes_resolution import DetectionBoundingBoxSize
-from .classes_count import DetectionClassesCount
+from .classes_count import DetectionObjectsPerClass
 from .classes_heatmap_per_class import DetectionClassHeatmap
 from .classes_per_image_count import DetectionClassesPerImageCount
 from .sample_visualization import DetectionSampleVisualization
@@ -11,7 +11,7 @@ __all__ = [
     "DetectionBoundingBoxArea",
     "DetectionBoundingBoxPerImageCount",
     "DetectionBoundingBoxSize",
-    "DetectionClassesCount",
+    "DetectionObjectsPerClass",
     "DetectionClassHeatmap",
     "DetectionClassesPerImageCount",
     "DetectionSampleVisualization",
