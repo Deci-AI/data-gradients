aea8346e00c6ec2ca9b823a310d4399414af6de3
Louis-Dupont <35190946+Louis-Dupont@users.noreply.github.com>
2023-07-05 16:27:34 +0300

    fix (#123)

--- src/data_gradients/feature_extractors/segmentation/bounding_boxes_area.py
+++ src/data_gradients/feature_extractors/segmentation/bounding_boxes_area.py
@@ -43,7 +43,7 @@ class SegmentationBoundingBoxArea(AbstractFeatureExtractor):
         max_area = min(100, df["bbox_area"].max())
         plot_options = ViolinPlotOptions(
             x_label_key="bbox_area",
-            x_label_name="Bounding Box Area (in % of image)",
+            x_label_name="Object Area (in % of image)",
             y_label_key="class_name",
             y_label_name="Class",
             order_key="class_id",
