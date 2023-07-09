8a5bb1d365ac926a3a824846182ca89e7811d03a
Louis Dupont <louis-dupont@live.fr>
2023-07-09 12:11:45 +0300

    rmove unwanted changes

--- src/data_gradients/visualize/detection/detection_legend.py
+++ src/data_gradients/visualize/detection/detection_legend.py
@@ -13,16 +13,24 @@ MARGIN_SPACE = 20
 
 @dataclass
 class LabelInfo:
-    """Holds information about labels.
+    """Hold information about labels.
 
-    :param name: Label name.
-    :param color: Color of the label.
-    :param text_size: Size of the label text.
+    :attr name: Label name.
+    :attr color: Color of the label.
+    :attr text_size: Size of the label text.
     """
 
     name: str
     color: Tuple[int, int, int]
     text_size: Tuple[int, int]
+
+
+@dataclass
+class Row:
+    """Represent a row of labels."""
+
+    labels: List[LabelInfo]
+    total_width: int
 
 
 def get_text_size(text: str) -> Tuple[int, int]:
@@ -44,7 +52,7 @@ def get_label_info(name: str, color: Tuple[int, int, int]) -> LabelInfo:
     return LabelInfo(name, color, get_text_size(name))
 
 
-def add_to_row_or_create_new(rows: List[List[LabelInfo]], label: LabelInfo, image_width: int) -> List[List[LabelInfo]]:
+def add_to_row_or_create_new(rows: List[Row], label: LabelInfo, image_width: int) -> List[Row]:
     """Adds a label to a row or creates a new row if the current one is full.
 
     :param rows: Existing rows of labels.
@@ -52,11 +60,13 @@ def add_to_row_or_create_new(rows: List[List[LabelInfo]], label: LabelInfo, imag
     :param image_width: Width of the image.
     :return: Updated rows of labels.
     """
-    if not rows or rows[-1][-1] + label.text_size[0] + 2 * MARGIN_SPACE > image_width:
-        rows.append([label, label.text_size[0] + 2 * MARGIN_SPACE])
+    if not rows or rows[-1].total_width + label.text_size[0] + 2 * MARGIN_SPACE > image_width:
+        # create a new row and initialize total width
+        rows.append(Row([label], label.text_size[0] + 2 * MARGIN_SPACE))
     else:
-        rows[-1].append(label)
-        rows[-1][-1] += label.text_size[0] + MARGIN_SPACE
+        # append label to existing row and add to total width
+        rows[-1].labels.append(label)
+        rows[-1].total_width += label.text_size[0] + MARGIN_SPACE
     return rows
 
 
@@ -70,7 +80,7 @@ def get_sorted_labels(class_color_tuples: Sequence[Tuple[str, Tuple[int, int, in
     return [get_label_info(name, color) for name, color in sorted_classes]
 
 
-def get_label_rows(labels: List[LabelInfo], image_width: int) -> List[List[LabelInfo]]:
+def get_label_rows(labels: List[LabelInfo], image_width: int) -> List[Row]:
     """Arranges labels in rows to fit into the image.
 
     :param labels: List of labels.
@@ -104,7 +114,7 @@ def draw_legend_on_canvas(image: np.ndarray, class_color_tuples: Iterable[Tuple[
 
     :param image: The image to draw the legend on.
     :param class_color_tuples: Iterable of tuples containing class name and its color.
-    :return: The canvas with the legend drawn on it.
+    :return: The canvas with the legend drawnOops, it seems like the response got cut off.
     """
     sorted_labels = get_sorted_labels(class_color_tuples)
     label_rows = get_label_rows(sorted_labels, image.shape[1])
@@ -116,7 +126,7 @@ def draw_legend_on_canvas(image: np.ndarray, class_color_tuples: Iterable[Tuple[
 
     for row in label_rows:
         horizontal_position = MARGIN_SPACE
-        for label in row[:-1]:
+        for label in row.labels:
             canvas, horizontal_position = draw_label_on_canvas(canvas, label, (horizontal_position, vertical_position), INITIAL_FONT_SIZE)
         vertical_position += sorted_labels[0].text_size[1] + MARGIN_SPACE
 
