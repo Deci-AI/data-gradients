7208a95050419b3e5262e85bba0213221c43abdd
Louis Dupont <louis-dupont@live.fr>
2023-07-12 12:36:56 +0300

    add test batch

--- tests/unit_tests/datasets/segmentation/test_coco_segmentation_dataset.py
+++ tests/unit_tests/datasets/segmentation/test_coco_segmentation_dataset.py
@@ -1,5 +1,5 @@
 import unittest
-
+from torch.utils.data import DataLoader
 from data_gradients.managers.segmentation_manager import SegmentationAnalysisManager
 from data_gradients.datasets.segmentation.coco_segmentation_dataset import CocoSegmentationDataset
 
@@ -14,6 +14,19 @@ class CocoSegmentationDatasetTest(unittest.TestCase):
             report_title="Testing Data-Gradients NEW",
             train_data=self.train_set,
             val_data=self.val_set,
+            class_names=self.train_set.class_names,
+            batches_early_stop=10,
+            use_cache=False,
+        )
+
+        da.run()
+
+    def test_coco_dataset_batch(self):
+
+        da = SegmentationAnalysisManager(
+            report_title="Testing Data-Gradients NEW",
+            train_data=DataLoader(self.train_set, batch_size=1),
+            val_data=DataLoader(self.val_set, batch_size=1),
             class_names=self.train_set.class_names,
             batches_early_stop=10,
             use_cache=False,
