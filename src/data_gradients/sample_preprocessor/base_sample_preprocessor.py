bb1f147e2401085a28606286de79e356314caf19
Louis Dupont <louis-dupont@live.fr>
2023-09-18 18:46:37 +0300

    minor fix

--- src/data_gradients/sample_preprocessor/base_sample_preprocessor.py
+++ src/data_gradients/sample_preprocessor/base_sample_preprocessor.py
@@ -7,15 +7,8 @@ from data_gradients.config.data.data_config import DataConfig
 
 
 class BaseSamplePreprocessor(ABC):
-    """
-    Iterate over a dataset adapter and yield Sample objects one at a time.
-    """
-
     def __init__(self, config: DataConfig):
         self.config = config
-
-    def close(self):
-        self.config.close()
 
     @abstractmethod
     def preprocess_samples(self, dataset: Iterable[SupportedDataType], split: str) -> Iterator[ImageSample]:
