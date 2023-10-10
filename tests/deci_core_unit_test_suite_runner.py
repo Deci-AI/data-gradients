444f3abf6e61cd236841ce37618dac32e9852ae6
Louis Dupont <louis-dupont@live.fr>
2023-10-10 15:32:00 +0300

    add test

--- tests/deci_core_unit_test_suite_runner.py
+++ tests/deci_core_unit_test_suite_runner.py
@@ -1,8 +1,9 @@
 import sys
 import unittest
 
-from tests.unit_tests.average_brightness_test import AverageBrightnessTest
-from tests.unit_tests.feature_extractors.detection.test_bounding_boxes_area import TestComputeHistogram
+from unit_tests.average_brightness_test import AverageBrightnessTest
+from unit_tests.feature_extractors.detection.test_bounding_boxes_area import TestComputeHistogram
+from unit_tests.dataset_output_mapper import TestImageConverter
 
 
 class CoreUnitTestSuiteRunner:
@@ -10,8 +11,6 @@ class CoreUnitTestSuiteRunner:
         self.test_loader = unittest.TestLoader()
         self.unit_tests_suite = unittest.TestSuite()
         self._add_modules_to_unit_tests_suite()
-        self.end_to_end_tests_suite = unittest.TestSuite()
-        self._add_modules_to_end_to_end_tests_suite()
         self.test_runner = unittest.TextTestRunner(verbosity=3, stream=sys.stdout)
 
     def _add_modules_to_unit_tests_suite(self):
@@ -21,6 +20,7 @@ class CoreUnitTestSuiteRunner:
         """
         self.unit_tests_suite.addTest(self.test_loader.loadTestsFromModule(AverageBrightnessTest))
         self.unit_tests_suite.addTest(self.test_loader.loadTestsFromModule(TestComputeHistogram))
+        self.unit_tests_suite.addTest(self.test_loader.loadTestsFromModule(TestImageConverter))
 
 
 if __name__ == "__main__":
