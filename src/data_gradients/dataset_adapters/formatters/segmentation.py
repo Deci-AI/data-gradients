f0d719996088fad563f08b133964e6beba281683
Louis Dupont <louis-dupont@live.fr>
2023-10-25 10:57:19 +0200

    refacto the formatter class

--- src/data_gradients/dataset_adapters/formatters/segmentation.py
+++ src/data_gradients/dataset_adapters/formatters/segmentation.py
@@ -51,74 +51,67 @@ class SegmentationBatchFormatter(BatchFormatter):
         if not self.check_is_batch(images=images, labels=labels):
             images = images.unsqueeze(0)
             labels = labels.unsqueeze(0)
-        # We have images/labels of shape (BS, ...)
 
-        images = ensure_channel_first(images, n_image_channels=self.get_n_image_channels(images=images))
-        images = check_images_shape(images, n_image_channels=self.get_n_image_channels(images=images))
-
-        if 0 <= images.min() and images.max() <= 1:
-            images *= 255
-            images = images.to(torch.uint8)
-        elif images.min() < 0:  # images were normalized with some unknown mean and std
-            images -= images.min()
-            images /= images.max()
-            images *= 255
-            images = images.to(torch.uint8)
-
-        # Here, We have labels of shape (BS, ...)
-        labels = labels.squeeze()  # Avoid dim of size 1
-        if labels.ndim == 3:
-            # Case if classes are floats (e.g. [0.2, 0.6, 0.8] -> [1, 3, 4] because it was normalized by factor of 5
-            labels = self.ensure_hard_labels(labels, n_classes=self.data_config.get_n_classes(), threshold_value=self.threshold_value)
-        elif labels.ndim == 4:
-            n_classes = self.data_config.get_n_classes()
-            # if (BS, C, H, W), -> (BS, H, W, C)
-            if labels.shape[1] == n_classes:
-                labels.permute(0, 3, 1, 2)
-
-            # To categorical (BS, H, W, C), -> (BS, H, W)
-            labels = torch.argmax(labels, dim=1)
-        # We have labels of shape (Bs, H, W)
+        images = self._format_images(images)
+        labels = self._format_labels(labels)
 
         for class_id_to_ignore in self.class_ids_to_ignore:
-            labels[labels == class_id_to_ignore] = -1  # Set the ignored class labels to -1 or any other value indicating 'ignore'.
+            labels[labels == class_id_to_ignore] = -1
 
         return images, labels
 
     def check_is_batch(self, images: Tensor, labels: Tensor) -> bool:
         if images.ndim == 4 or labels.ndim == 4:
-            # if less any dim is 4, we know it's a batch
             self.data_config.is_batch = True
-            return self.data_config.is_batch
         elif images.ndim == 2 or labels.ndim == 2:
-            # If image or mask only includes 2 dims, we can guess it's a single sample
             self.data_config.is_batch = False
-            return self.data_config.is_batch
         else:
-            # Otherwise, we need to ask the user
-            hint = f"    - Image shape: {images.shape}\n    - Mask shape:  {labels.shape}"
-            return self.data_config.get_is_batch(hint=hint)
+            hint = f"Image shape: {images.shape}\nMask shape: {labels.shape}"
+            self.data_config.is_batch = self.data_config.get_is_batch(hint=hint)
+        return self.data_config.is_batch
 
-    @staticmethod
-    def ensure_hard_labels(labels: Tensor, n_classes: int, threshold_value: float) -> Tensor:
-        unique_values = torch.unique(labels)
+    def _format_images(self, images: Tensor) -> Tensor:
+        images = ensure_channel_first(images, n_image_channels=self.get_n_image_channels(images=images))
+        images = check_images_shape(images, n_image_channels=self.get_n_image_channels(images=images))
+        images = adjust_image_values(images)
+        return images
 
-        if check_all_integers(unique_values):
-            return labels
-        elif 0 <= min(unique_values) and max(unique_values) <= 1 and check_all_integers(unique_values * 255):
-            return labels * 255
+    def _format_labels(self, labels: Tensor) -> Tensor:
+        labels = labels.squeeze((1, -1))  # If (BS, 1, H, W) or (BS, H, W, 1) -> (BS, H, W)
+        if labels.ndim == 3:
+            labels = ensure_hard_labels(labels, n_classes=self.data_config.get_n_classes(), threshold_value=self.threshold_value)
+        elif labels.ndim == 4:
+            labels = convert_to_categorical(labels, n_classes=self.data_config.get_n_classes())
         else:
-            if n_classes > 1:
-                raise DatasetFormatError(f"Not supporting soft-labeling for number of classes > 1!\nGot {n_classes} classes.")
-            labels = SegmentationBatchFormatter.binary_mask_above_threshold(labels=labels, threshold_value=threshold_value)
+            raise DatasetFormatError(f"Labels should be either 3D (categorical) or 4D (onehot), but got {labels.ndim}D")
         return labels
 
-    @staticmethod
-    def binary_mask_above_threshold(labels: Tensor, threshold_value: float) -> Tensor:
-        # Support only for binary segmentation
-        labels = torch.where(
-            labels > threshold_value,
-            torch.ones_like(labels),
-            torch.zeros_like(labels),
-        )
+
+def adjust_image_values(images: Tensor) -> Tensor:
+    if 0 <= images.min() and images.max() <= 1:
+        return (images * 255).to(torch.uint8)
+    elif images.min() < 0:
+        images = (images - images.min()) / images.max() * 255
+        return images.to(torch.uint8)
+    return images
+
+
+def ensure_hard_labels(labels: Tensor, n_classes: int, threshold_value: float) -> Tensor:
+    unique_values = torch.unique(labels)
+    if check_all_integers(unique_values):
         return labels
+    elif 0 <= min(unique_values) and max(unique_values) <= 1 and check_all_integers(unique_values * 255):
+        return labels * 255
+    elif n_classes == 1:
+        return binary_mask_above_threshold(labels, threshold_value)
+    raise DatasetFormatError(f"Not supporting soft-labeling for number of classes > 1! Got {n_classes} classes.")
+
+
+def convert_to_categorical(labels: Tensor, n_classes: int) -> Tensor:
+    if labels.shape[1] == n_classes:
+        labels = labels.permute(0, 2, 3, 1)  # (BS, C, H, W) -> (BS, H, W, C)
+    return torch.argmax(labels, dim=-1)
+
+
+def binary_mask_above_threshold(labels: Tensor, threshold_value: float) -> Tensor:
+    return torch.where(labels > threshold_value, torch.ones_like(labels), torch.zeros_like(labels))
