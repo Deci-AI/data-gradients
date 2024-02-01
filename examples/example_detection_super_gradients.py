a42c6235acedac6195ece7ed8aac905c4d0717fa
shayaharon <shay.aharon@deci.ai>
2024-02-01 14:46:25 +0200

    reverted example debug changes

--- examples/example_detection_super_gradients.py
+++ examples/example_detection_super_gradients.py
@@ -9,28 +9,22 @@ pip install super-gradients
 """
 
 # Note: This example will require you to install the super-gradients package
-
+from super_gradients.training.dataloaders.dataloaders import coco2017_train, coco2017_val
 from data_gradients.managers.detection_manager import DetectionAnalysisManager
-from data_gradients.datasets.detection.coco_detection_dataset import COCODetectionDataset
-from data_gradients.utils.data_classes.image_channels import BGRChannels
+
 
 if __name__ == "__main__":
-    train_set = COCODetectionDataset(root_dir="/data/coco", split="train", year="2017")
-    val_set = COCODetectionDataset(root_dir="/data/coco", split="val", year="2017")
 
-    # %%
+    train_loader = coco2017_train()
+    val_loader = coco2017_val()
 
-    manager = DetectionAnalysisManager(
-        report_title="Detection Coco 2000 10 iou thresh",
-        train_data=train_set,
-        val_data=val_set,
-        class_names=["background"] + train_set.class_names,
-        is_label_first=True,
-        bbox_format="xywh",
-        image_channels=BGRChannels("BGR"),
-        batches_early_stop=2000,
+    analyzer = DetectionAnalysisManager(
+        report_title="Testing Data-Gradients",
+        train_data=train_loader,
+        val_data=val_loader,
+        class_names=train_loader.dataset.classes,
+        batches_early_stop=20,
+        use_cache=True,  # With this we will be asked about the dataset information only once
     )
 
-    # %%
-
-    manager.run()
+    analyzer.run()
