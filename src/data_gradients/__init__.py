77611af479bf3117c13b2cfb7bf5bc441787d11d
CircleCI commit <circleci@deci.ai>
2024-02-29 19:11:57 +0000

    Deci Services - Changed version to 0.4.0rc2646

--- src/data_gradients/__init__.py
+++ src/data_gradients/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.2"
+__version__ = "0.4.0rc2646"
 
 from .managers.detection_manager import DetectionAnalysisManager
 from .managers.classification_manager import ClassificationAnalysisManager
